d62097dee131ded416d437c521734c823e27bbf2
dlwfuture <dlw_future@hotmail.com>
2018-08-18 12:33:18 -0300

    Initial commit

---
+++ balancedBrackets.js
@@ -0,0 +1,37 @@
+class BalancedBrackets {
+    static AllowOnlySpecificChars(str, chars) {
+        return str && str.length ? str.split('')
+            .filter(s => chars.indexOf(s) > -1)
+            .join('') : str
+    }
+
+    static Validate(str){
+        const openBrackets = ['[','{','(']
+        const closeBrackets = [']','}',')']
+        const allBrackets = [...openBrackets,...closeBrackets]
+        const onlyBracketsStr = this.AllowOnlySpecificChars(str, allBrackets)
+        let notBalancedBrackets = []
+    
+        if (!onlyBracketsStr || !onlyBracketsStr.length) {
+            return true
+        }
+    
+        if (onlyBracketsStr.length % 2 !== 0){
+            return false
+        }
+    
+        for (const ch of onlyBracketsStr) {
+            if (closeBrackets.indexOf(ch) === -1){
+                notBalancedBrackets.push(ch)
+            }
+            else{
+                const match = openBrackets[closeBrackets.indexOf(ch)]
+                if (notBalancedBrackets.length === 0 || (notBalancedBrackets.pop() !== match)) {
+                    return false
+                }
+            }
+        }
+        
+        return notBalancedBrackets.length == 0
+    }   
+}
---
+++ balancedBracketsTests.js
@@ -0,0 +1,20 @@
+class BalancedBracketsTests{
+    static Validate(testNumber, actual, expected) {
+        const passed = actual === expected
+        passed && console.info(`Test n.${testNumber} passed`)
+        !passed && console.error(`
+        Test n.${testNumber} failed
+        Expected: ${JSON.stringify(expected)}
+        Actual: ${JSON.stringify(actual)}
+        `)
+        return passed
+    }
+    
+    
+    static Test(){
+        this.Validate(1, BalancedBrackets.Validate("(){}[]"), true)
+        this.Validate(2, BalancedBrackets.Validate("[{()}](){}"), true)
+        this.Validate(3, BalancedBrackets.Validate("[]{()"), false)
+        this.Validate(4, BalancedBrackets.Validate("[{)]"), false)
+    }
+}
