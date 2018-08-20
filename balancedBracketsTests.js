c11f30da823adcaab0bfe1572c3e73d408d53106
dlwfuture <dlw_future@hotmail.com>
2018-08-19 22:28:02 -0300

    Improvements on test

--- balancedBracketsTests.js
+++ balancedBracketsTests.js
@@ -1,21 +1,31 @@
 const BalancedBrackets = require('./balancedBrackets')
 
 module.exports = {
-    Validate: function(testNumber, actual, expected) {
+    Validate: function(testNumber, actual, expected, expression) {
         const passed = actual === expected
-        passed && console.info(`Test n.${testNumber} passed`)
+        passed && console.info(`
+        Test n.${testNumber} passed
+        Expected: ${JSON.stringify(expected)}
+        Actual: ${JSON.stringify(actual)}
+        Value: ${JSON.stringify(expression)}
+        `)
         !passed && console.error(`
         Test n.${testNumber} failed
         Expected: ${JSON.stringify(expected)}
         Actual: ${JSON.stringify(actual)}
+        Value: ${JSON.stringify(expression)}
         `)
         return passed
     },
 
     Test: function(){
-        this.Validate(1, BalancedBrackets.Validate("(){}[]"), true)
-        this.Validate(2, BalancedBrackets.Validate("[{()}](){}"), true)
-        this.Validate(3, BalancedBrackets.Validate("[]{()"), false)
-        this.Validate(4, BalancedBrackets.Validate("[{)]"), false)
+        let testVal = "(){}[]"
+        this.Validate(1, BalancedBrackets.Validate(testVal), true, testVal)
+        testVal = "[{()}](){}"
+        this.Validate(2, BalancedBrackets.Validate(testVal), true, testVal)
+        testVal = "[]{()"
+        this.Validate(3, BalancedBrackets.Validate(testVal), false, testVal)
+        testVal = "[{)]"
+        this.Validate(4, BalancedBrackets.Validate(testVal), false, testVal)
     }
 }
